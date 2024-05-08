8f10a31ed59a637917d3264ad7e64fbee070880c
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-05-08 23:15:47 +0700

    exception

---
+++ src/main/java/com/project/BankingApplication/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.project.BankingApplication.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> notFoundExceptionHandler(NotFoundException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("error: ", e.getMessage());
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+}
