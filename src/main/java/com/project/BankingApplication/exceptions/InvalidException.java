6deae0a6d447032e529ad0d64e2a8061b5da8350
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-05-09 01:06:53 +0700

    handle invalid exception

---
+++ src/main/java/com/project/BankingApplication/exceptions/InvalidException.java
@@ -0,0 +1,7 @@
+package com.project.BankingApplication.exceptions;
+
+public class InvalidException extends Exception {
+    public InvalidException(String message) {
+        super(message);
+    }
+}
