4ada781cd017a98f882a96ef6e7f1994e3ba4e48
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-05-08 23:21:37 +0700

    my controller

---
+++ src/main/java/com/project/BankingApplication/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.project.BankingApplication.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+
+public class UserController {
+}
---
+++ src/main/java/com/project/BankingApplication/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.BankingApplication.repositories;
+
+import com.project.BankingApplication.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
+}
