b3dee7bf120c0cb883c5ed4838a97966f0cbcb7c
BinhPhuc <phamphucbinh_hsgs2021@hus.edu.vn>
2024-05-09 01:06:22 +0700

    login request

--- src/main/java/com/project/BankingApplication/controllers/UserController.java
+++ src/main/java/com/project/BankingApplication/controllers/UserController.java
@@ -1,10 +1,30 @@
 package com.project.BankingApplication.controllers;
 
+import com.project.BankingApplication.dtos.LoginDTO;
+import com.project.BankingApplication.dtos.RegisterDTO;
+import com.project.BankingApplication.entities.User;
+import com.project.BankingApplication.exceptions.InvalidException;
+import com.project.BankingApplication.exceptions.NotFoundException;
+import com.project.BankingApplication.services.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 
 public class UserController {
+    private final IUserService userService;
+    @PostMapping("/api/v1/register")
+    public ResponseEntity<?> register (@RequestBody RegisterDTO registerDTO) throws NotFoundException {
+        User user = userService.register(registerDTO);
+        return ResponseEntity.ok(user);
+    }
+    @PostMapping("/api/v1/login")
+    public ResponseEntity<?> login (@RequestBody LoginDTO loginDTO) throws NotFoundException, InvalidException {
+        String token = userService.login(loginDTO);
+        return ResponseEntity.ok(token);
+    }
 }
